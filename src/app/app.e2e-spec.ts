6b2ca54e8d1e0e1cee7ebc8d5d140a37c7b32a00
Narr <nardgu@gmail.com>
2016-06-18 23:07:02 +0900

    add e2e test

--- src/app/app.e2e-spec.ts
+++ src/app/app.e2e-spec.ts
@@ -8,4 +8,19 @@ describe('/', () => {
     const result = 'NARR';
     expect(subject).toEqual(result);
   });
+
+  it('should have active class after Contact, one of Sidebar menus is clicked and' +
+    'its animation is finished', done => {
+      const ANI_DURATION = 2000;
+      const contact = element(by.css('narr-sidebar li:nth-child(4)'));
+      contact.click();
+      setTimeout(() => {
+        contact.getAttribute('class').then(val => {
+          const subject = val;
+          const result = 'active';
+          expect(subject).toContain(result);
+          done();
+        });
+      }, ANI_DURATION);
+    });
 });
